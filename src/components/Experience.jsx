8a656551c04888f26f80db0a646e4b3f975ea704
StphnX <136083929+StphnX@users.noreply.github.com>
2023-10-20 23:26:08 +0200

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -62,8 +62,8 @@ function Experience() {
 
             <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
                 <div className='flex flex-col justify-center items-center'>
-                    <p className='text-6xl font-bold border-b-4 border-gray-500 p-2 inline'>Experience</p>
-                    <p className='py-6 text-2xl'>These are the technologies I've worked with</p>
+                    <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Experience</p>
+                    <p className='py-6 text-xl'>These are the technologies I've worked with</p>
                 </div>
 
                 <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
